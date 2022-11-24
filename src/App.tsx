7f6168ab1a4d1bd8daaf99a78f5cd9a30a9cff16
omer <oemer.peynirci@ontavio.de>
2022-11-24 10:44:15 +0100

    responsive design completed

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,23 @@ import Content from './components/content/Content';
 import ButtonRow from './components/buttonRow/ButtonRow';
 import Navbar from './components/navbar/Navbar';
 import styles from './App.module.css';
+
 function App() {
 
     return (
         <div className={styles.App}>
             <Title/>
-            <Navbar/>
-            <Content/>
+            <div className={styles.body}>
+                <div className={styles.navbar}>
+                    <Navbar/>
+
+                </div>
+                <div className={styles.content}>
+
+                    <Content/>
+                </div>
+            </div>
+
             <ButtonRow/>
         </div>
     );
